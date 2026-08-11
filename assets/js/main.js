1fe2e913358582bbdcf5d64eb13d3a0e35625cf1
Giovanni <louisgiovannir@gmail.com>
2026-08-11 14:40:51 +0300

    style: enable colorful portfolio refinement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+const vibrantTheme = document.createElement('link');
+vibrantTheme.rel = 'stylesheet';
+vibrantTheme.href = 'assets/css/vibrant.css';
+document.head.appendChild(vibrantTheme);
+
 const menuToggle = document.querySelector('.menu-toggle');
 const mainNav = document.querySelector('.main-nav');
 const navLinks = document.querySelectorAll('.main-nav a');
